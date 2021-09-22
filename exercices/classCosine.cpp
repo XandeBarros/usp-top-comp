12715f23e72b26d4964da9efea3d16b889976077
XandeBarros <barrosalex6@gmail.com>
2021-09-22 13:07:25 -0300

    Class Exercise Sine, Cosine and Tangent

---
+++ exercices/classCosine.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+class TrigonometricFunction {
+  private:
+    float cos;
+    float sin;
+    float tan;
+  
+    float myPow(float number, int exp) {
+      float result = 1;
+
+      for(int i = 0; i < exp; i++) {
+        result *= number;
+      }
+
+      return result;
+    }
+
+    int myFactorial(int number) {
+      int result = 1;
+
+      for(int i = number; i > 0; i--) {
+        result *= i;
+      }
+
+      return result;
+    }
+
+
+    void setSine() {
+      float evaluate = 1 - myPow(cos, 2);
+
+      sin = sqrt(evaluate);
+    }
+
+    void setTangent() {
+      tan = sin / (float)cos;
+    }
+
+  public:
+
+    void setCosine(float angle, int terms) {
+      cos = 1;
+      int exponent = 2;
+      int signal = -1;
+      float numerator = 0, denominator = 0, nthTerm;
+      for(int i = 0; i < terms; i++) {
+        numerator = myPow(angle, exponent);
+        denominator = myFactorial(exponent);
+        nthTerm = (numerator * signal) / (float)denominator;
+        cos += nthTerm;
+        signal *= -1;
+        exponent += 2;
+      }
+    }
+
+    float getSine() {
+      setSine();
+      return sin;
+    }
+
+    float getCosine() {
+      return cos;
+    }
+
+    float getTangent() {
+      setTangent();
+      return tan;
+    }
+};
+
+int main() {
+  TrigonometricFunction trigFuntions;
+  int terms = 0;
+  float angle = 0;
+
+  cout << "Digite o ângulo desejado em radianos: " << endl;
+  cin >> angle;
+  cout << "Digite a quantidade de termos da sequência: " << endl;
+  cin >> terms;
+
+  trigFuntions.setCosine(angle, terms);
+  cout << "SENO: " << setw(12) << trigFuntions.getSine() << endl;
+  cout << "COSSENO: " << setw(9) << trigFuntions.getCosine() << endl;
+  cout << "TANGENTE: " << setw(6) << trigFuntions.getTangent() << endl;
+
+  return 0;
+}
